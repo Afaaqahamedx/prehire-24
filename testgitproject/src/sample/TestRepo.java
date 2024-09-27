64f15284722a4fc7f62a60e1a85533ffdc27eff6
Afaaqahamedx <afaaqjamadar@gmail.com>
2024-09-27 10:30:44 +0530

    Adding my second commit

--- testgitproject/src/sample/TestRepo.java
+++ testgitproject/src/sample/TestRepo.java
@@ -4,6 +4,7 @@ public class TestRepo {
 
 	public static void main(String[] args) {
 		System.out.println("This is a sample GitRepo Example");
+		System.out.println("Adding my second line");
+		System.out.println("Adding my third line");
 	}
-
 }
